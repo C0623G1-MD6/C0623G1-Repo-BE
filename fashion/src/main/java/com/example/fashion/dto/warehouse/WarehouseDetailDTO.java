b8b240650bb289ab0132f703773dabc0bf79718f
TUẤN <tranvanlamqbdl@gmail.com>
2023-12-12 15:50:15 +0700

    lam up

--- fashion/src/main/java/com/example/fashion/dto/warehouse/WarehouseDetailDTO.java
+++ fashion/src/main/java/com/example/fashion/dto/warehouse/WarehouseDetailDTO.java
@@ -13,6 +13,8 @@ public class WarehouseDetailDTO implements Validator {
     private Integer productId;
     private Integer warehouseId;
     private static final String PRODUCT_ID_WAREHOUSE_DTO = "productId";
+    private static final String INPUT_QUANTITY = "inputQuantity";
+    private static final String INPUT_PRICE = "inputPrice";
 
     public String getReceiptCode() {
         return receiptCode;
@@ -66,10 +68,30 @@ public class WarehouseDetailDTO implements Validator {
     public boolean supports(Class<?> clazz) {
         return false;
     }
-
+    /**
+     * @author: LamTV
+     * @date: 12/12/2023
+     * @method : validation data
+     */
     @Override
     public void validate(Object target, Errors errors) {
         WarehouseDetailDTO warehouseDetailDTO = (WarehouseDetailDTO) target;
-
+        if (warehouseDetailDTO.getInputQuantity() == null){
+            errors.rejectValue(INPUT_QUANTITY, "", "Vui lòng không để trống số lượng");
+        }else if (warehouseDetailDTO.getInputQuantity() < 0) {
+            errors.rejectValue(INPUT_QUANTITY, "vui lòng nhập số lượng bé hơn 0");
+        } else if (warehouseDetailDTO.getInputQuantity() > 2000) {
+            errors.rejectValue(INPUT_QUANTITY, "Vui lòng nhập không quá 2000 sản phẩm");
+        }
+        if (warehouseDetailDTO.getInputPrice() == null){
+            errors.rejectValue(INPUT_PRICE, "", "Vui lòng không để trống đơn giá");
+        }else if (warehouseDetailDTO.getInputQuantity() < 0) {
+            errors.rejectValue(INPUT_PRICE, "vui lòng nhập đơn giá bé hơn 0");
+        } else if (warehouseDetailDTO.getInputQuantity() > 1000000000) {
+            errors.rejectValue(INPUT_PRICE, "Vui lòng nhập đơn giá không quá 1000000000");
+        }
+        if (warehouseDetailDTO.getProductId() == null) {
+            errors.rejectValue(PRODUCT_ID_WAREHOUSE_DTO, "", "Vui lòng không để trống sản phẩm");
+        }
     }
 }
