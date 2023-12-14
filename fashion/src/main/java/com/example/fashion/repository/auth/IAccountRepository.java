5ad0f7db31b27e39ae2e385d009715b7bc53ec76
phamthanhx18 <138504962+phamthanhx18@users.noreply.github.com>
2023-12-14 16:02:45 +0700

    thanh fix junit test

--- fashion/src/main/java/com/example/fashion/repository/auth/IAccountRepository.java
+++ fashion/src/main/java/com/example/fashion/repository/auth/IAccountRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -42,6 +43,7 @@ public interface IAccountRepository extends JpaRepository<Account,Long> {
      * @param username The username of the account.
      * @param password The new password to be set.
      */
+    @Transactional
     @Modifying
     @Query(value = "UPDATE accounts SET password = :password WHERE username = :username", nativeQuery = true)
     void updatePasswordAccount(@Param("username") String username, @Param("password") String password);
